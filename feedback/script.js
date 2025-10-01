94475051e86a1084e3449fb78bba9b2c10edf4f7
Saul Cetina <165978264+SaulCC23@users.noreply.github.com>
2025-09-30 20:08:44 -0600

    fisrt commit

---
+++ feedback/script.js
@@ -0,0 +1,4 @@
+document.getElementById("registerForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Formulario enviado (ejemplo). Aquí conectarías con Node.js o tu backend.");
+});
